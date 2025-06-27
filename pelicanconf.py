ba3556254e258efc2fd6c02f071960cdea70d4a4
taniomi <131834608+taniomi@users.noreply.github.com>
2025-06-27 16:08:19 -0300

    style: add logo

--- pelicanconf.py
+++ pelicanconf.py
@@ -1,40 +1,72 @@
+# ======================
+# Basic site settings
+# ======================
 AUTHOR = 'Taniomi'
 SITENAME = "Taniomi's Notebook"
 SITEURL = "https://taniomi.github.io/taniomis_notebook"
 
+# ======================
+# Paths and i18n
+# ======================
 PATH = "content"
-
+OUTPUT_PATH = "output/"
 TIMEZONE = 'America/Sao_Paulo'
-
 DEFAULT_LANG = 'en'
+DEFAULT_PAGINATION = 10
+RELATIVE_URLS = True
 
-# Feed generation is usually not desired when developing
+# ======================
+# Theme and appearance
+# ======================
+THEME = "themes/blue-penguin-dark"
+THEME_STATIC_DIR = 'static'
+
+# Theme-specific options
+SITELOGO = 'images/taniomi_logo.png'  # or '' if not used
+SITEDESCRIPTION = ''
+
+DISPLAY_HEADER = True
+DISPLAY_FOOTER = True
+DISPLAY_HOME = True
+DISPLAY_MENU = True
+DARK_LIGHT_SWITCHING_OFF = True
+
+MENU_INTERNAL_PAGES = (
+    ('Tags', 'tags', 'tags/index.html'),
+    ('Authors', 'authors', 'authors/index.html'),
+    ('Categories', 'categories', 'categories/index.html'),
+    ('Archives', 'archives', 'archives/index.html'),
+)
+MENUITEMS = (
+    ('GitHub', 'https://github.com/taniomi'),
+    ('Linux Kernel', 'https://www.kernel.org/'),
+)
+
+# Pagination URLs
+PAGINATION_PATTERNS = (
+    (1, '{url}', '{save_as}'),
+    (2, '{base_name}/page/{number}/', '{base_name}/page/{number}/index.html'),
+)
+
+# ======================
+# Feeds (disabled in dev)
+# ======================
 FEED_ALL_ATOM = None
 CATEGORY_FEED_ATOM = None
 TRANSLATION_FEED_ATOM = None
 AUTHOR_FEED_ATOM = None
 AUTHOR_FEED_RSS = None
 
-# Blogroll
+# ======================
+# Blogroll and Social
+# ======================
 LINKS = (
     ("Pelican", "https://getpelican.com/"),
     ("Python.org", "https://www.python.org/"),
     ("Jinja2", "https://palletsprojects.com/p/jinja/"),
     ("Pelican Themes", "https://pelicanthemes.com/"),
-    ("You can modify those links in your config file", "#"),
 )
-
-# Social widget
 SOCIAL = (
     ("GitHub", "https://github.com/taniomi"),
     ("LinkedIn", "https://linkedin.com/in/taniomi"),
 )
-
-DEFAULT_PAGINATION = 10
-
-# Theme
-THEME = "themes/blue-penguin-dark"
-THEME_STATIC_DIR = 'static'
-
-# Uncomment following line if you want document-relative URLs when developing
-RELATIVE_URLS = True
